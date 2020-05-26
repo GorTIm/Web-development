66d45fd9eb0fa5237ac9f2c52351c288480a785a
Tim Han <whan@ualberta.ca>
2020-05-25 21:24:14 -0600

    V2, implemented mongodb to store data

--- ChiveS/app.js
+++ ChiveS/app.js
@@ -1,11 +1,26 @@
-var express=require("express");
-var app=express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/chives",{useNewUrlParser:true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var shoesTemplate=new mongoose.Schema({
+	name:String,
+	image:String,
+	description:String,
+});
 
+var Shoes = mongoose.model("Shoes",shoesTemplate);
+
+
+/*
+Shoes.create(
+	{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",
+	description:"Sample description 1"}
+);
 
 var campgrounds = [
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
@@ -18,27 +33,51 @@ var campgrounds = [
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
 ];
+*/
     
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/shoes", function(req, res){
-    res.render("shoes",{campgrounds:campgrounds});
+	Shoes.find({},function(err,allShoes){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index",{shoes:allShoes});
+		}
+	});
 });
 
 app.post("/shoes", function(req, res){
     // get data from form and add to shoes array
     var name = req.body.name;
     var image = req.body.image;
-    var newShoes = {name: name, image: image}
-    campgrounds.push(newShoes);
+	var desc=req.body.description;
+    var newShoes = {name: name, image: image, description:desc}
     //redirect back to shoes page
-    res.redirect("/shoes");
+    Shoes.create(newShoes,function(err,newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/shoes");
+		}
+	});
 });
 
 app.get("/shoes/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("new"); 
+});
+
+app.get("/shoes/:id",function(req,res){
+	Shoes.findById(req.params.id,function(err,foundShoe){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("show",{shoe:foundShoe});
+		}
+	});
+	
 });
 
 app.listen(3000, function(){
